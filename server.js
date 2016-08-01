0c21379485163b402fb51af3f4c8f4040c62a305
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-08-01 18:07:52 +0530

    updating

---
+++ server.js
@@ -0,0 +1,58 @@
+var log = require('logger')('accounts-services');
+var nconf = require('nconf').argv().env();
+var async = require('async');
+var vhost = require('vhost');
+var mongoose = require('mongoose');
+var express = require('express');
+
+var mongourl = nconf.get('MONGODB_URI');
+
+var app = express();
+
+mongoose.connect(mongourl);
+
+var domains = ['accounts'];
+
+var clients = require('./package.json').clients;
+
+Object.keys(clients).forEach(function (name) {
+    nconf.set(name.toUpperCase() + '_CLIENT', clients[name]);
+});
+
+var db = mongoose.connection;
+db.on('error', function (err) {
+    log.error(err);
+});
+db.once('open', function () {
+    log.info('connected to mongodb');
+
+    var jobs = [];
+    domains.forEach(function (domain) {
+        jobs.push(function (done) {
+            var app = require(domain);
+            app(function (err, app) {
+                if (err) {
+                    return done(err);
+                }
+                done(null, {domain: domain, app: app});
+            });
+        });
+    });
+    async.parallel(jobs, function (err, o) {
+        if (err) {
+            throw err;
+        }
+        o.forEach(function (o) {
+            var host = o.domain + '.serandives.com';
+            app.use(vhost(host, o.app));
+            log.info('host %s was registered', host);
+        });
+        app.listen(nconf.get('PORT'));
+    });
+});
+
+process.on('uncaughtException', function (err) {
+    log.debug('unhandled exception ' + err);
+    log.debug(err.stack);
+    process.exit(1);
+});
