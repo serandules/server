e75ff2c8535a5181c21ae545271f7883e8f5a1e1
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-12-22 23:13:03 +0530

    updating

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var log = require('logger')('server:server');
 var nconf = require('nconf').argv().env();
+var fs = require('fs');
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
@@ -10,12 +11,28 @@ nconf.defaults(require('./env/' + env + '.json'));
 
 var server = require('./index');
 
+var certPath = nconf.get('CERT_SERANDIVES');
+var pemPath = nconf.get('PEM_SERVER');
 var mongourl = nconf.get('MONGODB_URI');
-mongoose.connect(mongourl, {useMongoClient: true});
+
+var ca = certPath ? fs.readFileSync(certPath) : null;
+var pem = pemPath ? fs.readFileSync(pemPath) : null;
+
+mongoose.connect(mongourl, {
+    useMongoClient: true,
+    authSource: 'admin',
+    ssl: !!pem,
+    sslCA: ca,
+    sslCert: pem,
+    sslKey: pem
+});
+
 var db = mongoose.connection;
+
 db.on('error', function (err) {
     log.error('mongodb connection error: %e', err);
 });
+
 db.once('open', function () {
     log.info('connected to mongodb');
     server.start(function (err) {
