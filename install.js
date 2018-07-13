877b2353b331bc128d1d32baee3082431e6811cf
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-07-13 11:59:30 +0530

    fixing bug

--- install.js
+++ install.js
@@ -12,7 +12,7 @@ var server = require('./index');
 server.install(function (err, modules) {
     if (err) {
         log.error(err);
-        throw e;
+        throw err;
     }
     log.info('modules were installed successfully');
 });
