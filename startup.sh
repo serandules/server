2f93a887e723e154c1ae689934329070aaaa7762
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-07-25 13:11:23 +0530

    updating

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 # initialize filebeat
 echo $LOGZIO_API_TOKEN | filebeat keystore add LOGZIO_API_TOKEN --stdin --force
-service filebeat start
+service filebeat start || true
 
 # start server
-npm start
+node server.js >> /var/log/server.log 2>&1
