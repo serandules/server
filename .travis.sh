68873118155256c55ded7e4bcd3ee77a5539bcc9
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-07-28 07:33:33 +0530

    updating

--- .travis.sh
+++ .travis.sh
@@ -4,17 +4,17 @@ SANDBOX=/tmp/server
 
 CURRENT=`pwd`
 
-echo $CURRENT
+echo "current directory ${CURRENT}"
 
 declare -a SERVICES=(`node -c << EOL
-var dev = require('./env/travis.json');
+var travis = require('./env/travis.json');
 var services = '';
-Object.keys(dev).forEach(function (name) {
+Object.keys(travis).forEach(function (name) {
     if (name.indexOf('SERVICE_') !== 0) {
         return;
     }
     var service = name.replace('SERVICE_', '').replace('_', '-').toLowerCase();
-    var val = dev[name].split(':');
+    var val = travis[name].split(':');
     var branch = val[0];
     services += services ? ' ' + service : service;
 });
@@ -26,10 +26,15 @@ mv package.json package.json-backup
 
 mkdir -p $SANDBOX
 cd $SANDBOX
+echo "sanbox directory ${SANDBOX}"
+
+echo $SERVICES
+
 rm -rf ./*
 
 for SERVICE in "${SERVICES[@]}"
 do
+    echo "cloning ${SERVICE}"
     git clone https://github.com/serandules/service-${SERVICE}.git
     if [ -d "service-${SERVICE}/test" ]; then
         mkdir -p ${CURRENT}/test/${SERVICE}
@@ -40,6 +45,8 @@ do
         cd $SANDBOX
     fi
 done
+
+echo "all repos cloned"
 
 cd $CURRENT
 
