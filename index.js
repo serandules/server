3cde6da22beca518964f3ac9711003df5d9dab82
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-09-13 08:51:50 +0530

    updating

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ var findServices = function () {
       type: 'service',
       name: type + '-' + name,
       version: splits[0],
-      domain: splits[1],
+      subdomain: splits[1],
       prefix: splits[2]
     });
   }
@@ -69,7 +69,7 @@ var findLocals = function () {
       type: 'local',
       name: type + '-' + name,
       path: splits[0],
-      domain: splits[1],
+      subdomain: splits[1],
       prefix: splits[2]
     });
   }
@@ -78,11 +78,26 @@ var findLocals = function () {
 
 var findClients = function () {
   var key;
-  var name;
+  var value;
+  var splits;
+  var subdomain;
   var o = [];
   var type = 'client';
-  var prefix = type.toUpperCase() + '_';
+  var prefix = type.toUpperCase();
   var all = nconf.get();
+
+  value = all[prefix];
+  if (value) {
+    splits = value.split(':');
+    o.push({
+      type: 'client',
+      name: splits[0],
+      version: splits[1],
+      subdomain: '',
+      prefix: '/'
+    });
+  }
+  prefix  += '_';
   for (key in all) {
     if (!all.hasOwnProperty(key)) {
       continue;
@@ -90,18 +105,24 @@ var findClients = function () {
     if (key.indexOf(prefix) !== 0) {
       continue;
     }
-    name = key.substring(prefix.length);
-    name = name.toLowerCase().replace('_', '-');
-    var value = all[key];
+    subdomain = key.substring(prefix.length);
+    subdomain = subdomain.toLowerCase().replace('_', '-');
+    value = all[key];
+    splits = value.split(':');
+
     o.push({
       type: 'client',
-      name: name,
-      version: value,
-      domain: name,
+      name: splits[0],
+      version: splits[1],
+      subdomain: subdomain,
       prefix: '/'
     });
   }
   return o;
+};
+
+var servicing = function (module) {
+  return module.type === 'local' || module.type === 'service';
 };
 
 var server;
@@ -134,9 +155,11 @@ exports.start = function (done) {
     if (err) {
       return done(err);
     }
-    var domains = {};
+    var subdomains = {};
     var apps = express();
-    var serverHost = nconf.get('SERVER_HOST');
+    var domain = utils.domain();
+    var subdomain = utils.subdomain();
+
     apps.use(morgan(':remote-addr :method :url :status :res[content-length] - :response-time ms'));
     apps.use(serandi.pond);
     apps.use(throttle.ips());
@@ -154,17 +177,17 @@ exports.start = function (done) {
       apps.use(serandi.ssl);
     }
     modules.forEach(function (module) {
-      var domain = domains[module.domain] || (domains[module.domain] = []);
-      domain.push(module);
+      var subdomain = subdomains[module.subdomain] || (subdomains[module.subdomain] = []);
+      subdomain.push(module);
     });
-    var i;
-    var host;
-    async.eachSeries(Object.keys(domains), function (domain, domainDone) {
+    async.eachSeries(Object.keys(subdomains), function (sub, subdomainDone) {
         var app = express();
-        var modulez = domains[domain];
+        var modulez = subdomains[sub];
         async.eachSeries(modulez, function (module, moduleDone) {
           var router = express();
-          router.use(serandi.locate(module.prefix + '/'));
+          if (servicing(module)) {
+            router.use(serandi.locate(module.prefix + '/'));
+          }
           var routes;
           try {
             routes = require(module.path || module.name);
@@ -176,17 +199,20 @@ exports.start = function (done) {
               return moduleDone(err);
             }
             app.use(module.prefix, router);
-            log.info('modules:registered', 'domain:%s name:%s type:%s', domain, module.name, module.type);
+            log.info('modules:registered', 'subdomain:%s name:%s type:%s', sub, module.name, module.type);
             moduleDone();
           });
         }, function (err) {
           if (err) {
-            return domainDone(err);
+            return subdomainDone(err);
           }
-          host = format(serverHost, {sub: domain});
+          var prefix = format(subdomain, {
+            subdomain: sub ? sub + '.' : ''
+          });
+          var host = prefix + domain;
           apps.use(vhost(host, app));
           log.info('hosts:registered', 'name:%s', host);
-          domainDone();
+          subdomainDone();
         });
     }, function (err) {
       if (err) {
